68988de4e27a2c2333b20e2314ccca1e631ce4a5
César Manuel Brito Orsinys <cesarorsinys@gmail.com>
2024-05-28 17:20:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,22 +14,12 @@ const entries = [
 
 // Inicializar variables
 let currentEntry = 0; // Índice de la entrada actual
-let scene, camera, renderer;
+let scene
 
 // Función para cargar y renderizar el modelo 3D
 function loadModel(entry) {
-    // Crear escena, cámara y renderer de Three.js
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-
-    // Configurar el renderer
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.getElementById('modelo-3d').appendChild(renderer.domElement);
-
-    // Cargar modelo 3D
-    const loader = new THREE.GLTFLoader();
-        loader.load(entry.modelUrl, (collada) => {
+    const loader = new THREE.ColladaLoader();
+    loader.load(entry.modelUrl, (collada) => {
         const scene = collada.scene;
         // Agregar la escena a tu escena Three.js
         scene.scale.set(0.01, 0.01, 0.01); // Ajustar la escala según sea necesario
